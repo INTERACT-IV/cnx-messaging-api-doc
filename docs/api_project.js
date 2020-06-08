15c95ebbf4ec8f16fc2f810fc1725234cb050d1d
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-06-08 18:13:11 +0200

    Update after review with FERA & JYSY

--- docs/api_project.js
+++ docs/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-06-08T13:35:51.069Z",
+    "time": "2020-06-08T16:12:41.110Z",
     "url": "http://apidocjs.com",
     "version": "0.23.0"
   }
