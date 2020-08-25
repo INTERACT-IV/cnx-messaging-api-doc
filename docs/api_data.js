97db9e230fab7ddbc79ad66ad914777f2ee5d11d
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-08-25 09:38:32 +0200

    Update

--- docs/api_data.js
+++ docs/api_data.js
@@ -11,34 +11,6 @@ define({ "api": [
     "groupTitle": "API_Architecture"
   },
   {
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "optional": false,
-            "field": "varname1",
-            "description": "<p>No type.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "varname2",
-            "description": "<p>With type.</p>"
-          }
-        ]
-      }
-    },
-    "type": "",
-    "url": "",
-    "version": "0.0.0",
-    "filename": "../cnx-messaging-server/docs/main.js",
-    "group": "C:\\workspace\\CNX\\cnx-messaging-server\\docs\\main.js",
-    "groupTitle": "C:\\workspace\\CNX\\cnx-messaging-server\\docs\\main.js",
-    "name": ""
-  },
-  {
     "type": "post",
     "url": "/messages",
     "title": "Broadcasting messages",
